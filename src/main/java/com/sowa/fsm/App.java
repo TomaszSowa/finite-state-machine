d2dc6338565c2b35845f280a796e16a33eb4ebbd
TomaszSowa <tsowa974@gmail.com>
2018-03-11 03:51:13 +0100

    Add State class.

--- src/main/java/com/sowa/fsm/App.java
+++ src/main/java/com/sowa/fsm/App.java
@@ -1,13 +1,20 @@
 package com.sowa.fsm;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Starting FSM...");
+
+        /* test
+        String[] splitted = "Split,split2,split3".split(",", 2);
+        for (String str : splitted) System.out.println(str);
+        Scanner sc = new Scanner(System.in);
+        String aa = sc.nextLine();
+        System.out.println("'" + aa + "'");
+        */
+
     }
 }
---
+++ src/main/java/com/sowa/fsm/State.java
@@ -0,0 +1,26 @@
+package com.sowa.fsm;
+
+public class State {
+
+    private String id;
+    private boolean accepting = false;
+    private String[] sets;
+
+    public State(String id, boolean accepting, String[] sets) {
+        this.id = id;
+        this.accepting = accepting;
+        this.sets = sets;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isAccepting() {
+        return accepting;
+    }
+
+    public String[] getSets() {
+        return sets;
+    }
+}
