da0d27d48ddef29065839b86f84bccf04fc54cd8
TomaszSowa <tsowa974@gmail.com>
2018-03-11 04:00:11 +0100

    Add Graph class.

---
+++ src/main/java/com/sowa/fsm/Graph.java
@@ -0,0 +1,9 @@
+package com.sowa.fsm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graph {
+
+    private List<State> graph = new ArrayList<>();
+}
--- src/main/java/com/sowa/fsm/State.java
+++ src/main/java/com/sowa/fsm/State.java
@@ -23,4 +23,5 @@ public class State {
     public String[] getSets() {
         return sets;
     }
+
 }
