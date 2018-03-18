9bc45c5b39a3c347e3a042154efc137bffd12ae3
tom <tom@eee-Fedora>
2018-03-18 10:42:02 +0100

    Add testing case.

--- src/main/java/com/sowa/fsm/App.java
+++ src/main/java/com/sowa/fsm/App.java
@@ -8,8 +8,10 @@ public class App {
     public static void main(String[] args) {
 
         final String PATH = "./src/main/resources/";
-        final String TRANSITIONS_FILE = "even0bin.txt";
-        final String SEQUENCEFILE_FILE = "01.txt";
+//        final String TRANSITIONS_FILE = "even0bin.txt";
+//        final String SEQUENCEFILE_FILE = "01.txt";
+        final String TRANSITIONS_FILE = "bbab.txt";
+        final String SEQUENCEFILE_FILE = "ab.txt";
 
         Graph graph = new Graph(PATH + TRANSITIONS_FILE);
 
